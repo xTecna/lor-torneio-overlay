1525b36becbc8f6af0bad74928e5952fb637cf9d
xTecna <raquelmarcolino25@gmail.com>
2021-03-13 11:05:09 -0300

    Novas imagens trazidas pela Riot, acabou a gambiarra.

--- frontend/src/components/Overlay/Jogador/Deck/index.js
+++ frontend/src/components/Overlay/Jogador/Deck/index.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React from "react";
 
-import {Deck as DeckDiv, Region, Champion} from './style';
+import { Deck as DeckDiv, Region, Champion } from "./style";
 
-function renderRegion(region, index, status){
-	return <Region className={`region ${status}`} key={index}><img src={region === 'shurima' ? 'http://escolaruneterra.com.br/gallerycard/assets/icon-shurima.png' : `http://dd.b.pvp.net/latest/core/en_us/img/regions/icon-${region}.png`} alt={region}/></Region>;
-};
+function renderRegion(region, index, status) {
+  return (
+    <Region className={`region ${status}`} key={index}>
+      <img
+        src={`http://dd.b.pvp.net/latest/core/en_us/img/regions/icon-${region}.png`}
+        alt={region}
+      />
+    </Region>
+  );
+}
 
-function renderChampion(champion, index, status){
-	return <Champion className={`champion ${status}`} key={index} img={`http://ddragon.leagueoflegends.com/cdn/10.15.1/img/champion/${champion.nome}.png`}>{champion.qtd}</Champion>;
-};
+function renderChampion(champion, index, status) {
+  return (
+    <Champion
+      className={`champion ${status}`}
+      key={index}
+      img={`http://ddragon.leagueoflegends.com/cdn/10.15.1/img/champion/${champion.nome}.png`}
+    >
+      {champion.qtd}
+    </Champion>
+  );
+}
 
-const Deck = ({status, deck}) => {
-	const {regions, champions} = deck;
+const Deck = ({ status, deck }) => {
+  const { regions, champions } = deck;
 
-	return (
-		<DeckDiv className={`deck ${status}`}>
-			{champions.length !== 0 ?
-				champions.map((item, index) => renderChampion(item, index, status)) :
-				regions.map((item, index) => renderRegion(item, index, status))
-			}
-		</DeckDiv>
-	)
+  return (
+    <DeckDiv className={`deck ${status}`}>
+      {champions.length !== 0
+        ? champions.map((item, index) => renderChampion(item, index, status))
+        : regions.map((item, index) => renderRegion(item, index, status))}
+    </DeckDiv>
+  );
 };
 
 export default Deck;
--- frontend/src/components/Overlay/Jogador/RegionsChampions/index.js
+++ frontend/src/components/Overlay/Jogador/RegionsChampions/index.js
@@ -1,29 +1,64 @@
-import React from 'react';
+import React from "react";
 
-import {RegionsChampions as RegionsChampionsDiv, Regions, Region, Champions, Champion} from './style';
+import {
+  RegionsChampions as RegionsChampionsDiv,
+  Regions,
+  Region,
+  Champions,
+  Champion,
+} from "./style";
 
-function renderRegion(region, index){
-	return <Region className="region" key={index}><img src={region === 'shurima' ? 'http://escolaruneterra.com.br/gallerycard/assets/icon-shurima.png' : `http://dd.b.pvp.net/latest/core/en_us/img/regions/icon-${region}.png`} alt={region}/></Region>;
-};
+function renderRegion(region, index) {
+  return (
+    <Region className="region" key={index}>
+      <img
+        src={`http://dd.b.pvp.net/latest/core/en_us/img/regions/icon-${region}.png`}
+        alt={region}
+      />
+    </Region>
+  );
+}
 
-function renderChampion(champion, index, number_champions){
-	return <Champion className="champion" key={index} lotado={number_champions > 4} img={`http://ddragon.leagueoflegends.com/cdn/10.15.1/img/champion/${champion.nome}.png`} alt={champion.nome}>{champion.qtd}</Champion>;
-};
+function renderChampion(champion, index, number_champions) {
+  return (
+    <Champion
+      className="champion"
+      key={index}
+      lotado={number_champions > 4}
+      img={`http://ddragon.leagueoflegends.com/cdn/10.15.1/img/champion/${champion.nome}.png`}
+      alt={champion.nome}
+    >
+      {champion.qtd}
+    </Champion>
+  );
+}
 
-const RegionsChampions = ({inline, time, regions, champions}) => {
-	const number_champions = champions.length;
+const RegionsChampions = ({ inline, time, regions, champions }) => {
+  const number_champions = champions.length;
 
-	return (
-		<RegionsChampionsDiv className="regions-champions" inline={inline} time={time}>
-			<Regions className="regions" inline={inline} time={time} nochampions={champions.length === 0}>
-				{regions.map(renderRegion)}
-			</Regions>
-			{champions.length !== 0 &&
-			<Champions className="champions" inline={inline} time={time}>
-				{champions.map((item, index) => renderChampion(item, index, number_champions))}
-			</Champions>}
-		</RegionsChampionsDiv>
-	);
+  return (
+    <RegionsChampionsDiv
+      className="regions-champions"
+      inline={inline}
+      time={time}
+    >
+      <Regions
+        className="regions"
+        inline={inline}
+        time={time}
+        nochampions={champions.length === 0}
+      >
+        {regions.map(renderRegion)}
+      </Regions>
+      {champions.length !== 0 && (
+        <Champions className="champions" inline={inline} time={time}>
+          {champions.map((item, index) =>
+            renderChampion(item, index, number_champions)
+          )}
+        </Champions>
+      )}
+    </RegionsChampionsDiv>
+  );
 };
 
 export default RegionsChampions;
