2b5e59f42842ac91b5ecdac00952087d75a30438
xTecna <raquelmarcolino25@gmail.com>
2021-04-03 18:42:53 -0300

    Consertado o erro do Riotlock.

--- frontend/src/components/Form/JanelaErro/index.js
+++ frontend/src/components/Form/JanelaErro/index.js
@@ -1,28 +1,43 @@
-import React from 'react';
-import {CgCloseO} from 'react-icons/cg';
+import React from "react";
+import { CgCloseO } from "react-icons/cg";
 
-import {JanelaErro as JanelaErroDiv, MensagensErro, MensagemErroDetalhes, MensagemErro, Detalhes, BotaoFechar} from './style';
+import {
+  JanelaErro as JanelaErroDiv,
+  MensagensErro,
+  MensagemErroDetalhes,
+  MensagemErro,
+  Detalhes,
+  BotaoFechar,
+} from "./style";
 
-const JanelaErro = ({erros, setErros}) => {
-	return (
-		<JanelaErroDiv>
-			<MensagensErro>
-				{erros.map((erro) => {
-					if (erro.detalhes){
-						return (
-							<MensagemErroDetalhes>
-								<MensagemErro>{erro.mensagem}</MensagemErro>
-								<Detalhes href={erro.detalhes} target="_blank" rel="noopener noreferrer">Mais Detalhes</Detalhes>
-							</MensagemErroDetalhes>
-						);
-					}else{
-						return <MensagemErro>{erro.mensagem}</MensagemErro>
-					}
-				})}
-			</MensagensErro>
-			<BotaoFechar onClick={() => setErros([])}><CgCloseO/></BotaoFechar>
-		</JanelaErroDiv>
-	);
+const JanelaErro = ({ erros, setErros }) => {
+  return (
+    <JanelaErroDiv>
+      <MensagensErro>
+        {erros.map((erro, index) => {
+          if (erro.detalhes) {
+            return (
+              <MensagemErroDetalhes key={index}>
+                <MensagemErro>{erro.mensagem}</MensagemErro>
+                <Detalhes
+                  href={erro.detalhes}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Mais Detalhes
+                </Detalhes>
+              </MensagemErroDetalhes>
+            );
+          } else {
+            return <MensagemErro>{erro.mensagem}</MensagemErro>;
+          }
+        })}
+      </MensagensErro>
+      <BotaoFechar onClick={() => setErros([])}>
+        <CgCloseO />
+      </BotaoFechar>
+    </JanelaErroDiv>
+  );
 };
 
 export default JanelaErro;
--- frontend/src/components/Form/SectionParticipantes/index.js
+++ frontend/src/components/Form/SectionParticipantes/index.js
@@ -1,102 +1,141 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import {useSaveState} from '../../../context/SaveState';
+import { useSaveState } from "../../../context/SaveState";
 
-import SectionTitle from '../SectionTitle';
-import {Section, SectionContent, PesquisarImportar} from '../style';
-import BarraPesquisar from '../BarraPesquisar';
-import ImportacaoParticipantes from './ImportacaoParticipantes';
-import JanelaErro from '../JanelaErro';
-import TabelaParticipantes from './TabelaParticipantes';
-import FormularioParticipante from './FormularioParticipante';
+import SectionTitle from "../SectionTitle";
+import { Section, SectionContent, PesquisarImportar } from "../style";
+import BarraPesquisar from "../BarraPesquisar";
+import ImportacaoParticipantes from "./ImportacaoParticipantes";
+import JanelaErro from "../JanelaErro";
+import TabelaParticipantes from "./TabelaParticipantes";
+import FormularioParticipante from "./FormularioParticipante";
 
 const SectionParticipantes = () => {
+  const regraFuncao = {
+    Cardlock: checkCardlock,
+    Regionlock: checkRegionlock,
+    Riotlock: checkRiotlock,
+  };
 
-	const regraFuncao = {
-		'Cardlock': checkCardlock,
-		'Regionlock': checkRegionlock,
-		'Riotlock': checkRiotlock
-	};
+  function checkCardlock(decks) {
+    const allCardCodes = decks.flatMap((deck) =>
+      deck.cards.map((card) => card.code)
+    );
+    const cards = [...new Set(allCardCodes)];
 
-	function checkCardlock(decks){
-		const allCardCodes = decks.flatMap((deck) => deck.cards.map((card) => card.code));
-		const cards = [...new Set(allCardCodes)];
+    return allCardCodes.length === cards.length;
+  }
 
-		return allCardCodes.length === cards.length;
-	}
-	
-	function checkRegionlock(decks){
-		const allRegions = decks.flatMap((deck) => deck.regions);
-		const regions = [...new Set(allRegions)];
-	
-		return allRegions.length === regions.length;
-	}
-	
-	function checkRiotlock(decks){
-		const allChampions = decks.flatMap((deck) => deck.champions);
-		const champions = [...new Set(allChampions)];
+  function checkRegionlock(decks) {
+    const allRegions = decks.flatMap((deck) => deck.regions);
+    const regions = [...new Set(allRegions)];
 
-		const allRegions = decks.map((deck) => deck.regions.join(''));
-		const regions = [...new Set(allRegions)];
+    return allRegions.length === regions.length;
+  }
 
-		return (allChampions.length === champions.length) && (allRegions.length === regions.length);
-	}
+  function checkRiotlock(decks) {
+    const allChampions = decks.flatMap((deck) =>
+      deck.champions.flatMap((champion) => champion.nome)
+    );
+    const champions = [...new Set(allChampions)];
 
-	const { saveState } = useSaveState();
-	const { jogadores } = saveState;
+    console.log(allChampions);
+    console.log(champions);
 
-	const [ mostrar, setMostrar ] = useState(true);
-	const [ jogadorQuery, setJogadorQuery ] = useState('');
-	const [ mostrarEditar, setMostrarEditar ] = useState(false);
-	const [ jogadorAntigo, setJogadorAntigo ] = useState({nome: '',
-		time: {nome: '', url_logo: ''},
-		decks: [{code: '', regions: [], champions: []},
-				{code: '', regions: [], champions: []},
-				{code: '', regions: [], champions: []}]});
+    const allRegions = decks.map((deck) => deck.regions.sort().join(""));
+    const regions = [...new Set(allRegions)];
 
-	const [ errosImportacao, setErrosImportacao ] = useState([]);
+    console.log(allRegions);
+    console.log(regions);
 
-	function buscaJogador(nome){
-		return jogadores.find((jogador) => jogador.nome === nome);
-	}
+    return (
+      allChampions.length === champions.length &&
+      allRegions.length === regions.length &&
+      decks.reduce(
+        (acc, current) => (current.champions.length === 0 ? acc + 1 : acc),
+        0
+      ) < 2
+    );
+  }
 
-	function apresentaFormulario(nome){
-		const jogador = buscaJogador(nome);
+  const { saveState } = useSaveState();
+  const { jogadores } = saveState;
 
-		if (jogador){
-			setJogadorAntigo(jogador);
-			setMostrarEditar(true);
-		}
-	}
+  const [mostrar, setMostrar] = useState(true);
+  const [jogadorQuery, setJogadorQuery] = useState("");
+  const [mostrarEditar, setMostrarEditar] = useState(false);
+  const [jogadorAntigo, setJogadorAntigo] = useState({
+    nome: "",
+    time: { nome: "", url_logo: "" },
+    decks: [
+      { code: "", regions: [], champions: [] },
+      { code: "", regions: [], champions: [] },
+      { code: "", regions: [], champions: [] },
+    ],
+  });
 
-	return (
-		<Section>
-			<SectionTitle mostrar={mostrar} toggleMostrar={setMostrar}>
-				Participantes
-			</SectionTitle>
-			{mostrar &&
-				<SectionContent>
-					<PesquisarImportar>
-						<BarraPesquisar name={'jogador'} query={jogadorQuery} funcaoMuda={setJogadorQuery}/>
-						<ImportacaoParticipantes regraFuncao={regraFuncao} funcaoErro={setErrosImportacao}/>
-					</PesquisarImportar>
-					{errosImportacao.length > 0 && <JanelaErro erros={errosImportacao}
-															   setErros={setErrosImportacao}/>}
-					<TabelaParticipantes jogadorQuery={jogadorQuery} apresentaFormulario={apresentaFormulario}/>
-					{mostrarEditar &&
-						<FormularioParticipante titulo={`Editar Participante ${jogadorAntigo.nome}`}
-							mensagemClica='Editar jogador' regraFuncao={regraFuncao}
-							mostrar={mostrarEditar} setMostrar={setMostrarEditar}
-							jogadorAntigo={jogadorAntigo}/>
-					}
-					<FormularioParticipante titulo='Novo Participante'
-							mensagemClica='Cadastrar novo jogador' regraFuncao={regraFuncao}
-							mostrar={undefined} setMostrar={undefined}
-							jogadorAntigo={undefined}/>
-				</SectionContent>
-			}
-		</Section>
-	);
+  const [errosImportacao, setErrosImportacao] = useState([]);
+
+  function buscaJogador(nome) {
+    return jogadores.find((jogador) => jogador.nome === nome);
+  }
+
+  function apresentaFormulario(nome) {
+    const jogador = buscaJogador(nome);
+
+    if (jogador) {
+      setJogadorAntigo(jogador);
+      setMostrarEditar(true);
+    }
+  }
+
+  return (
+    <Section>
+      <SectionTitle mostrar={mostrar} toggleMostrar={setMostrar}>
+        Participantes
+      </SectionTitle>
+      {mostrar && (
+        <SectionContent>
+          <PesquisarImportar>
+            <BarraPesquisar
+              name={"jogador"}
+              query={jogadorQuery}
+              funcaoMuda={setJogadorQuery}
+            />
+            <ImportacaoParticipantes
+              regraFuncao={regraFuncao}
+              funcaoErro={setErrosImportacao}
+            />
+          </PesquisarImportar>
+          {errosImportacao.length > 0 && (
+            <JanelaErro erros={errosImportacao} setErros={setErrosImportacao} />
+          )}
+          <TabelaParticipantes
+            jogadorQuery={jogadorQuery}
+            apresentaFormulario={apresentaFormulario}
+          />
+          {mostrarEditar && (
+            <FormularioParticipante
+              titulo={`Editar Participante ${jogadorAntigo.nome}`}
+              mensagemClica="Editar jogador"
+              regraFuncao={regraFuncao}
+              mostrar={mostrarEditar}
+              setMostrar={setMostrarEditar}
+              jogadorAntigo={jogadorAntigo}
+            />
+          )}
+          <FormularioParticipante
+            titulo="Novo Participante"
+            mensagemClica="Cadastrar novo jogador"
+            regraFuncao={regraFuncao}
+            mostrar={undefined}
+            setMostrar={undefined}
+            jogadorAntigo={undefined}
+          />
+        </SectionContent>
+      )}
+    </Section>
+  );
 };
 
 export default SectionParticipantes;
